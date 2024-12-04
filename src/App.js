f0ceaa323de4eec0843e6bca63572e543b432008
Prabuddha Sonalkar <sonalkarprabuddha5565@gmail.com>
2024-12-04 12:32:29 +0530

    file added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='form'>
+      <h1>Login form</h1>
+      <form>
+        <label>Enter username:
+          <input type='text'/>
+        </label>
+        <br/>
+        <label>Enter password:
+          <input type='text'/>
+        </label>
+        <br/>
+        <button onClick={alert}>Submit</button>
+
+        <script>
+          function alert(){
+            alert("Form Submitted")
+          }
+        </script>
+      </form>
+      </div>
     </div>
   );
 }
+
 
 export default App;
